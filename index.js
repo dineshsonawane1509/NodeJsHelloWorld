4bdffb024aa7fb6a57b636bba12cb2925f015d19
Sonawane <Sonawane.Dinesh@principal.com>
2020-01-07 13:10:13 +0530

    Update Docker File

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const http = require('http');
+var http = require("http");
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
